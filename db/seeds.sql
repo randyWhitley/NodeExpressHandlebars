c4fb9a83c34b437dc92561b552bf7ffa9597bce6
Randy Whitley <randywhitley84@gmail.com>
2021-01-10 13:33:35 -0600

    baseline SQL and server.js code

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+("Carolina Burger", false),
+("Peanut Butter Burger", true),
+("Gator Burger", false);
---
+++ server.js
@@ -0,0 +1,27 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+// port
+let PORT = process.env.PORT || 3306;
+
+// 
+app.use(express.static("public")); 
+// 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// the handlebars
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import route and give the server acces to them.
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start server for listening.
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
