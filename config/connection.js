86c498690528ab679e3e5b9f4bf840eba3289ac4
Randy Whitley <randywhitley84@gmail.com>
2021-01-16 14:43:27 -0600

    Connection?

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,17 @@ const mysql = require("mysql");
 let connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+} else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "fruit",
-    database: "burgers_db"
+    database: "burgers_db",
   });
-};
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
